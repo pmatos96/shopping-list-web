1ed2b79430d909ef849e0706964903d3f7e517db
Patrick Matos <pmatos96@hotmail.com>
2023-10-04 20:55:47 -0300

    feat: go to list items page by button

--- src/components/ShoppingListSelection.tsx
+++ src/components/ShoppingListSelection.tsx
@@ -2,6 +2,7 @@ import { Icon, Segment } from "semantic-ui-react";
 import React from "react";
 import { ShoppingList } from "../types/shoppingListTypes";
 import MainApi from "../apis/mainApi";
+import { useNavigate } from "react-router-dom";
 
 type ShoppingListSelection = ShoppingList & {
     deleteList: Function
@@ -9,13 +10,20 @@ type ShoppingListSelection = ShoppingList & {
 
 const ShoppingListSelection = ({ name, id, deleteList }: ShoppingListSelection) => {
 
+    const navigate = useNavigate();
+
     const handleDeleteButtonClick = () => {
         deleteList(id);
     }
 
+    const goToListItemsPage = () => {
+
+        navigate(`/lista/${id}`)
+    }
+
     return (
         <Segment className="flex justify-between">
-            {name}
+            <span className="cursor-pointer" onClick={goToListItemsPage}>{name}</span>
             <Icon name="trash alternate" onClick={handleDeleteButtonClick} />
         </Segment>
     )
