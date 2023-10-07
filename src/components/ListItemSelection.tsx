abe9b3a5c79091427b0cd15474b5e15c9138cc64
Patrick Matos <pmatos96@hotmail.com>
2023-10-07 09:36:09 -0300

    feat: list item selection layout

--- src/components/ListItemSelection.tsx
+++ src/components/ListItemSelection.tsx
@@ -1,13 +1,33 @@
 import React from "react";
-import { Checkbox, Segment } from "semantic-ui-react";
+import { Button, Checkbox, Icon, Input, Label, Segment } from "semantic-ui-react";
 import { ListItem } from "../types/shoppingListTypes";
 
 const ListItemSelection = ({ id, product, amount }: ListItem) => {
 
     return (
-        <Segment className="flex justify-between">
-            <span>{product.name}</span>
-            <Checkbox />
+        <Segment className="flex justify-between items-center">
+            <div>
+                <Checkbox />
+                <span>{product.name}</span>
+            </div>
+            <div className="flex justify-center">
+                <Input
+                    className="w-[17%]"
+                    type="text"
+                    value={amount}
+                />
+                <Button.Group size="mini">
+                    <Button size="mini" icon>
+                        <Icon name='plus' />
+                    </Button>
+                    <Button size="mini" icon>
+                        <Icon name='minus' />
+                    </Button>
+                    <Button size="mini" icon>
+                        <Icon name='trash alternate' />
+                    </Button>
+                </Button.Group>
+            </div>
         </Segment>
     )
 }
